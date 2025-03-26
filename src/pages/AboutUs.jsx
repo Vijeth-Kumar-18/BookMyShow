de1d9b8153c89cf1756231bcae8dd5e10e337ea5
Vijeth-Kumar-18 <vijethkumar3110@gmail.com>
2025-03-26 23:45:30 +0530

    Updated AboutUs

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Card } from 'react-bootstrap';
+import { Row, Col} from 'react-bootstrap';
 
 
 const AboutUs = () => {
@@ -13,17 +13,6 @@ const AboutUs = () => {
             Welcome to BookMyShow Clone! Our mission is to provide you with a seamless experience in browsing and booking tickets for movies, events, and sports. 
           </p>
         </Col>
-        <Col md={6}>
-          <Card>
-            <Card.Img variant="top" src="https://via.placeholder.com/300" alt="Our Mission" />
-            <Card.Body>
-              <Card.Title>Our Vision</Card.Title>
-              <Card.Text>
-                To be the leading platform for entertainment booking, ensuring customer satisfaction and convenience.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
       </Row>
       <Row>
         <Col md={6}>
@@ -31,17 +20,6 @@ const AboutUs = () => {
           <p>
             Built using React and Bootstrap, this application aims to mimic the functionalities and design of the original BookMyShow platform. We offer a user-friendly interface, a wide variety of options, and excellent customer support to ensure you have the best experience possible.
           </p>
-        </Col>
-        <Col md={6}>
-          <Card>
-            <Card.Img variant="top" src="https://via.placeholder.com/300" alt="What We Offer" />
-            <Card.Body>
-              <Card.Title>Our Services</Card.Title>
-              <Card.Text>
-                Explore a wide range of movies, events, and sports, all at your fingertips.
-              </Card.Text>
-            </Card.Body>
-          </Card>
         </Col>
       </Row>
       <Row>
